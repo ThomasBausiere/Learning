6b1dc81bd5b83c0d1a07e07acea5af3907a7235a
Thomas.bausiere <t.bausiere@gmail.com>
2021-04-05 18:43:42 +0200

    cour 2

--- Cours.js
+++ Cours.js
@@ -7,3 +7,28 @@
 // 3; la valeur de celle ci
 //
 //
+
+
+
+// OBJETS : un objet est un assemblage complexe ( d'un ou plusieurs type ) de variables
+
+//Les Classes
+
+// exemple :
+// let episode = {
+//     title: "Jpp",
+//     duration: 60,
+//     hasBeenWatched: "true",
+//   };
+
+
+//acceder à ces informations;
+
+//dot notation
+
+// let DurationEpisode = episode.duration; 
+
+//bracket notation
+
+//let DurationEpisode =episode["duration"]; 
+
