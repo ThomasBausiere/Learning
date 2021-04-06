e222f76d8aa687c5080c9da1439446c86f70f8c0
Thomas.bausiere <t.bausiere@gmail.com>
2021-04-06 14:13:38 +0200

    ajout cours sur tableau

--- Cours.js
+++ Cours.js
@@ -32,3 +32,43 @@
 
 //let DurationEpisode =episode["duration"]; 
 
+// Le Array
+
+//Pour créer un tableau vide et l'enregistrer dans une variable, utilisez une paire de crochets :
+// let guests = [];
+
+//Vous pouvez aussi créer un tableau rempli en plaçant les éléments voulus à l'intérieur de ces crochets :
+
+// let guests = ["Sarah Kate", "Audrey Simon", "Will Alexander"];
+
+// Vous pouvez ensuite accéder aux éléments de ce tableau par leur indice :
+
+// let firstGuest = guests[0]; // "Sarah Kate"
+
+ 
+
+// let thirdGuest = guests[2]; // "Will Alexander"
+
+ 
+
+// let undefinedGuest = guests[12] // undefined
+
+
+// Le comptage d'éléments
+// La propriété   length  d'un tableau indique le nombre d'éléments qu'il contient
+
+
+// L'ajout et la suppression d'éléments
+// Pour ajouter un élément à la fin d'un tableau, utilisez sa méthode push :
+
+// guests.push("Tao Perkington");
+
+// Pour ajouter votre élément au début du tableau plutôt qu'à la fin, utilisez la méthode   unshift 
+
+//guests.unshift("Tao Perkington"); 
+
+//Pour supprimer le dernier élément d'un tableau, appelez sa méthode  pop  , sans passer aucun argument :
+
+// guests.pop(); 
+
+
